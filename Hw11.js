317810f4ab7448e2ec5eada488fc590d6fc60196
tbdmb41 <32739958+tbdmb41@users.noreply.github.com>
2018-02-21 12:01:08 -0500

    Add files via upload

---
+++ Hw11.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    $("#btnExit").click(function () {
+        $("#page").fadeOut(2000);
+        $("#goodbye").delay(1000).show(2000);
+
+function convertToC() {
+    var far = parseFloat(document.getElementById('far').value);
+    var cel = (fTempVal - 32) * (5 / 9);
+    document.getElementById('cel').value = cel;
+    return false;
+}
+
+function convertToF() {
+    var cTempVal = parseFloat(document.getElementById('c').value);
+    var fTempVal = (cTempVal * (9 / 5)) + 32;
+    console.log(f);
+    document.getElementById('f').value = f;
+    return false;
+}
+
+console.reset = function () {
+    return getElementById();
+}
+
+function convertToC() {
+    try {
+        var c = (fTempVal - 32) * (5 / 9);
+        if (!isNaN(g) && document.getElementById('Celsius').checked === true) {
+            return value g; document.getElementByID("Temp").innerHTML = "Must be an integer";
+            return false;
+        } else {
+            throw new Error('Response is not a number')
+        }
+        catch (ex) { }
+        finally {
+            log(c.name + '' + c.message);
+            return 'We are unable to get you the correct temperature value. Please use numbers insteas of letters';
+
+            function convertToF() {
+                try {
+                    var f = (cTempVal * (9 / 5)) + 32;
+                    if (!isNaN(f)) {
+                        return f;
+                    } else {
+                        throw new Error('Response is not a number');
+                    } try { f }
+                    finally {
+                        log(f.name + '' + f.message);
+                        return 'We are unable to get you the correct temperature value. Please use numbers instead of letters';
+
+                        form; action_page.php
+                        type; reset, value = "Try Again";
+                        if ('button onclick = myFunction() Exit')
+                            return null;
+                    }else{
+                        throw new error('Action events are not correct')
+                    } try { onmouseover } finally {
+                        var console = f, string = '' + f.message
+                            ;
+                        return 'We are unable to give you a value. Please try again';
+                    }
+
+                    try { fadeTarget } finally { '' };
+                    var fadeEffect = '',
+                    try {
+                        if (try) { opacity < 10 }
+                        return opacity > 10;
+                    }
+
+                    finally { opacity < 10 };
+                    return (opacity > 10);
+                } else (opacity > 10);
+                throw new Error('The parameters that have been used do not go with the equation. Please re-evaluate script.')
+
+
+                $('#submit').on('click', function () {
+                    if ($('#container').css('opacity') == 0) {
+                        $('#container').css('opacity', 1);
+                    }
+                    else {
+                        $('#container').css('opacity', 0);
+                    }
+                });
+            }
+    /* not sure on this one.. The fade out is not working so I am not sure what to write the error codes about.*/
+
+
+result; { };
+clearSelection()
+
+
+
